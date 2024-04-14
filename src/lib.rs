2a718d6a62ee251f687aa29e2f1c2ebd401a77c4
Johannes Erwerle <jo@swagspace.org>
2024-04-14 11:47:54 +0200

    added example for the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,38 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-//! Struct and methods for union-find operation.
+//! An implementation of the Union-Find datastructure.
+//!
+//! This crate implements 2 variations of the union find datastructure.
+//! `QuickFindUf` implements an always O(1) `find` while `QuickUnionUf` has a
+//! `union` operation that is always O(1).
+//!
+//! There is also the option whether the union operation joins the underlying
+//! tree structure by size or by rank via the `UnionBySize`, `UnionByRank`,
+//! `UnionByRankSize` and `UnionBySizeRank` structs that need to be passed
+//! to the Union Find datastructure.
+//!
+//! ```
+//! use union_find::{UnionFind, UnionBySize, QuickUnionUf};
+//!
+//! // build a union find datastructure for 10 elements with quick unions,
+//! // merge the unions by size.
+//! let mut uf = QuickUnionUf::<UnionBySize>::new(10);
+//!
+//! // initially each element is in it's own set
+//! for i in 0..10 {
+//!     assert_eq!(uf.find(i), i);
+//! }
+//!
+//! // join sets containing 0 and 1
+//! assert!(uf.union(0,1));
+//!
+//! assert_eq!(uf.find(0), 0);
+//! assert_eq!(uf.find(1), 0);
+//! for i in 2..10 {
+//!     assert_eq!(uf.find(i), i);
+//! }
+//! ```
 
 #![warn(missing_copy_implementations)]
 #![warn(missing_debug_implementations)]
